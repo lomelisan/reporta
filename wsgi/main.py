94f36928c13f2d15fcea2f4e4b13387bdceb3e92
lomelisan <lomelisan@gmail.com>
2015-08-18 15:18:18 -0430

    edition xlsx file

--- wsgi/main.py
+++ wsgi/main.py
@@ -402,8 +402,8 @@ def processing():
 	global filepath
 	datafile = file(filepath)
 	patternFilePath = os.path.join(application.config['UPLOAD_FOLDER'], "test.xlsx")
-	a1ApzPathZip = os.path.join(application.config['UPLOAD_FOLDER'], "a1Apz.txt")
-	heirFilePath = os.path.join(application.config['UPLOAD_FOLDER'], "test2.xlsx")
+	a1ApzPathZip = os.path.join(application.config['UPLOAD_FOLDER'], "reporta/a1Apz.txt")
+	heirFilePath = os.path.join(application.config['UPLOAD_FOLDER'], "reporta/test2.xlsx")
 	countApzA1 = 0
 	countApzA2 = 0
 	countApzA3 = 0
