063a0b18cf0b45425c1e5065c4c569a1314dce54
lomelisan <lomelisan@gmail.com>
2015-08-18 08:25:17 -0430

    xlsx file

--- wsgi/main.py
+++ wsgi/main.py
@@ -401,8 +401,8 @@ def upload():
 def processing():
 	global filepath
 	datafile = file(filepath)
-	#patternFilePath = os.path.join(application.config['UPLOAD_FOLDER'], "test.xlsx")
-	#heirFilePath = os.path.join(application.config['UPLOAD_FOLDER'], "test2.xlsx")
+	patternFilePath = os.path.join(application.config['UPLOAD_FOLDER'], "test.xlsx")
+	heirFilePath = os.path.join(application.config['UPLOAD_FOLDER'], "test2.xlsx")
 	countApzA1 = 0
 	countApzA2 = 0
 	countApzA3 = 0
@@ -443,12 +443,12 @@ def processing():
 			colApzA3.append(line)
 			
 	os.remove(filepath)
-	#wb = load_workbook(patternFilePath)
-	#ws = wb.get_sheet_by_name("mss")
+	wb = load_workbook(patternFilePath)
+	ws = wb.get_sheet_by_name("mss")
 	#c = ws.cell(row = 5, column = 5)
 	#c.hyperlink = (a1ApzPathXl)
-	#wb.save(heirFilePath)
-	#os.remove(heirFilePath)
+	wb.save(heirFilePath)
+	os.remove(heirFilePath)
 	return render_template('processing-results.html',countApzA1 = countApzA1,
 	 colApzA1=colApzA1, countApzA2=countApzA2, colApzA2=colApzA2 , 
 	 countApzA3=countApzA3, colApzA3 =colApzA3, page_title = 'Resultados' )
